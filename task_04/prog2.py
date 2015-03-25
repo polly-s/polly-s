1aa6723a84756e17d5d8eee5bb4e4576112caa39
polly-s <stognii@phystech.edu>
2015-03-25 16:22:39 +0300

    Create prog2.py

---
+++ task_04/prog2.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as pl
+data=np.genfromtxt("https://raw.githubusercontent.com/anaderi/lhcb_trigger_ml/master/hep_ml/datasets/dalitzplot/bkgd.csv"
+                   ,skip_header=1,delimiter="\t")
+
+pl.hist2d(data.T[0],data.T[1],bins=40,cmap=pl.cm.Blues)
+pl.xlabel('M2AC')
+pl.ylabel('M2AB')
+pl.colorbar()
+pl.show()
