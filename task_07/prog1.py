16fb43e9dc83ecb9cb4fff3c386464072b5f7a05
polly-s <stognii@phystech.edu>
2015-04-18 23:19:27 +0300

    Create prog1.py

---
+++ task_07/prog1.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys 
+from pandas import DataFrame, read_csv
+
+ratings=pd.read_table('./ratings.dat',header=None,delimiter='::',names=['UserID','MovieID','Rating','Timestamp'])
+movies=pd.read_table('./movies.dat',header=None,delimiter='::',names=['MovieID', 'Title', 'Genres'])
+users=pd.read_table('./users.dat',header=None,delimiter='::',names=['UserID','Gender','Age','Occupation','Zip-Code'])
+
+tab1=ratings[['MovieID','Rating']]
+tab2=movies[['MovieID','Title']]
+tab1=tab1.merge(tab2, how ='left',on ='MovieID')
+tab1 =tab1[['Title','Rating']]
+tab3= tab1.groupby('Title',as_index=False)
+tab1= tab3.sum()
+tab4= tab1.sort('Rating',ascending=False)
+tab4.head(25)
